ce5dfa9963c2af5bb5dfc55ea0b86071719158d6
VIVEK KUMAR <51090910+kvivek371@users.noreply.github.com>
2022-08-04 16:50:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-const express = require("express");
-const jsonData =require("./data")
+const MongoClient = require("mongodb").MongoClient
+const employeeData = require('./employee.json')
 
-const app = express();
+const connectionString = "mongodb://127.0.0.1:27017/mongo"
+MongoClient.connect(connectionString, async (err, db) => 
+{if (err) {
+    console.error("Error while connecting", err)
+    return}
+    const database = db.db("Human_Resoure")
+    const dbCollection = database.collection("employee")
 
-app.get('/user1',function(req,res){
-    // res.json(JSON.stringify({name:"rohit",class:"10th"}));
-    // res.send("hello world");
-    
-    // res.write("<h1>HELLO H1</h1>");
+    console.log("Connected to Mongo Database")
 
-    res.json(jsonData.rohit.json1);
+    const inserting = await dbCollection.insertMany(employeeData)
+    console.log(inserting)
+
+    const finding = await dbCollection.find().toArray()
+    console.log(finding)
+    console.log("showing all data")
+
+    const findSalary = await dbCollection.find({"salary":{$gt:"30000"}}).toArray()
+    console.log(findSalary)
+    console.log("showing all data those salary greater than 30000")
+
+    const findExp = await dbCollection.find({"overallExp":{$gt:"2"}}).toArray()
+    console.log(findExp)
+    console.log("showing all data those experience greater than 2")
+
+    const gradExp = await dbCollection.find({$and:[{"yearGrad":{$gt:"2015"}},{"overallExp":{$gt:"1"}}]}).toArray()
+    console.log(gradExp)
+    console.log("showing all data those graduated after 2015 and having exp greater than 1 ")
+
+    const updating = await dbCollection.updateMany({salary: {$gt:70000}},{$set: {salary: 65000}})
+    console.log(updating)
+
+    const deleting = await dbCollection.deleteMany({lastCompany: "Y" })
+    console.log(deleting)
+
 })
-
-app.get('/user2',function(req,res){
-    res.json(jsonData.rohit.json2);
-})
-// res.end();
-app.listen(3002);
